148ca7e9e59777bb45db9f193190e8b16865322d
Bill Wu <hi@bw5.io>
2021-01-31 18:00:59 +0000

    Password Change

--- blog/__init__.py
+++ blog/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)   
 app.config['SECRET_KEY']='2ea381c499e2df1774a2387309e4304579e3184bc143e629'
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://c2108464:48481BIye?@csmysql.cs.cf.ac.uk:3306/c2108464_cmt120flask'
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://c2108464:tuss1SAIN6boct_koh@csmysql.cs.cf.ac.uk:3306/c2108464_cmt120flask'
 
 db = SQLAlchemy(app)
 
