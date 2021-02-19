c4c171e4c9a7339f8aa0a9b8415c289bd12335d4
Bill Wu <wuh43@cardiff.ac.uk>
2021-02-19 08:24:05 +0000

    Revert "Merge branch 'revert-cda7417c' into 'master'"
    This reverts merge request !1

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_admin import Admin
+import os
 
 app = Flask(__name__)   
-app.config['SECRET_KEY']='2ea381c499e2df1774a2387309e4304579e3184bc143e629'
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://c2108464:tuss1SAIN6boct_koh@csmysql.cs.cf.ac.uk:3306/c2108464_cmt120flask'
+app.config['SECRET_KEY']=os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get("SQLALCHEMY_DATABASE_URI")
 
 db = SQLAlchemy(app)
 
